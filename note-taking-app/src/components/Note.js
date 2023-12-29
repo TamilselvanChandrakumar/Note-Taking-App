8b1427afff4da65f5aac6bf51d4893bbd3eb87c1
TamilselvanChandrakumar <tamilbin001@gmail.com>
2023-12-29 14:41:37 +0530

    first commit

--- note-taking-app/src/components/Note.js
+++ note-taking-app/src/components/Note.js
@@ -2,17 +2,58 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Note = () => {
-  const [note, setNote] = useState("");
+  const [notes, setNotes] = useState([]);
   const [newTitle, setNewTitle] = useState("");
   const [newNote, setNewNote] = useState("");
   const [newDate, setNewDate] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+  const [editMode, setEditMode] = useState(null);
+  const [editedContent, setEditedContent] = useState("");
   useEffect(() => {
     axios.get(" http://localhost:5000/notes").then((response) => {
-      setNote(response.data);
+      setNotes(response.data);
+      console.log(response);
+      console.log(response.data);
     });
   }, []);
-  const addnote = () => {};
+  const addnote = () => {
+    axios
+      .post("http://localhost:5000/notes", {
+        title: newTitle,
+        content: newNote,
+        date: newDate,
+      })
+      .then((response) => {
+        setNotes([...notes, response.data]);
+        setNewTitle("");
+        setNewNote("");
+        setNewDate("");
+      });
+  };
+  const deletenote = (id) => {
+    axios.delete("http://localhost:5000/notes/" + id).then(() => {
+      const updateNotes = notes.filter((note) => note.id !== id);
+      setNotes(updateNotes);
+    });
+  };
+  const updateNote = (id) => {
+    axios
+      .put("http://localhost:5000/notes/" + id, { content: editedContent })
+      .then(() => {
+        const updatedNotes = [...notes];
+        const notesIndex = updatedNotes.findIndex((note) => note.id === id);
+        updatedNotes[notesIndex].content = editedContent;
+        setNotes(updatedNotes);
+        setEditMode(null);
+      });
+  };
+  const filterNotes = searchTerm
+    ? notes.filter(
+        (note) =>
+          note.title &&
+          note.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+      )
+    : notes;
   return (
     <>
       <h1>Note</h1>
@@ -30,7 +71,7 @@ const Note = () => {
           onChange={(e) => setNewNote(e.target.value)}
         ></input>
         <input
-          type="data"
+          type="date"
           value={newDate}
           onChange={(e) => setNewDate(e.target.value)}
         ></input>
@@ -46,6 +87,28 @@ const Note = () => {
           onChange={(e) => setSearchTerm(e.target.value)}
         ></input>
       </div>
+      {filterNotes.map((note) => (
+        <div key={note.id}>
+          <h3>{note.title}</h3>
+          {editMode === note.id ? (
+            <div>
+              <input
+                type="text"
+                value={editedContent}
+                onChange={(e) => setEditedContent(e.target.value)}
+              ></input>
+              <button onClick={() => updateNote(note.id)}>update</button>
+            </div>
+          ) : (
+            <p>{note.content}</p>
+          )}
+          <p>{note.date}</p>
+          {editMode !== note.id ? (
+            <button onClick={(e) => setEditMode(note.id)}>Edit</button>
+          ) : null}
+          <button onClick={() => deletenote(note.id)}>delete</button>
+        </div>
+      ))}
     </>
   );
 };
